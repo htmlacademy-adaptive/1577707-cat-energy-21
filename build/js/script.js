b4f134a144c5c62143d19ec990cc45beee175bb5
вашÐАнтон <gluskov352@gmail.com>
2021-01-26 14:57:57 +0300

    Финальные правки

--- build/js/script.js
+++ build/js/script.js
@@ -9,34 +9,35 @@ btn.addEventListener('click', function() {
 });
 
 var submitForm = document.querySelector('.form__program');
-var nameError = document.querySelector('.name-error');
-var weightError = document.querySelector('.weight-error');
-var ageError = document.querySelector('.age-error');
 
-var emailError = document.querySelector('.form__details-input--email');
-var telError = document.querySelector('.form__details-input--tel');
+if (submitForm) {
+  const nameError = document.querySelector('.name-error');
+  const weightError = document.querySelector('.weight-error');
+  const ageError = document.querySelector('.age-error');
+  const emailError = document.querySelector('.form__details-input--email');
+  const telError = document.querySelector('.form__details-input--tel');
+  submitForm.addEventListener('submit', function(evt){
+    if(!nameError.value){
+      evt.preventDefault();
+      nameError.classList.add('general-input-error')
+    }
+    if(!weightError.value){
+      evt.preventDefault();
+      weightError.classList.add('general-input-error')
+    }
+    if(!ageError.value){
+      evt.preventDefault();
+      ageError.classList.add('general-input-error')
+    }
 
-submitForm.addEventListener('submit', function(evt){
-  if(!nameError.value){
-    evt.preventDefault();
-    nameError.classList.add('general-input-error')
-  }
-  if(!weightError.value){
-    evt.preventDefault();
-    weightError.classList.add('general-input-error')
-  }
-  if(!ageError.value){
-    evt.preventDefault();
-    ageError.classList.add('general-input-error')
-  }
+    if(!emailError.value){
+      evt.preventDefault();
+      emailError.classList.add('general-input-error')
+    }
 
-  if(!emailError.value){
-    evt.preventDefault();
-    emailError.classList.add('general-input-error')
-  }
-
-  if(!telError.value){
-    evt.preventDefault();
-    telError.classList.add('general-input-error')
-  }
-});
+    if(!telError.value){
+      evt.preventDefault();
+      telError.classList.add('general-input-error')
+    }
+  });
+}
